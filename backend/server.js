0f207494eec9a16de5699e0cae2268eb90af6581
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-08-13 07:04:35 +0530

    express server creation done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const dotenv = require('dotenv').config();
 const app = express();
 
-port = 5000
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
