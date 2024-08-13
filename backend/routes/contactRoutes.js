2db365010a77237b3f70df1320989270ab470ffb
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-08-13 07:39:39 +0530

    routes creation done

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router()
 
-// router.route('/').get((req, res) => {
-//   res.status(200).json({message: 'Get all contacts'})
-// });
-
 router.get('/', (req, res) => {
   res.status(200).json({message: 'Get all contacts'})
 });
-
+router.post('/', (req, res) => {
+  res.status(200).json({message: 'Create Contact'})
+});
+router.get('/:id', (req, res) => {
+  res.status(200).json({message: `get contact for ${req.params.id}`})
+});
+router.put('/:id', (req, res) => {
+  res.status(200).json({message: `update contact for ${req.params.id}`})
+}); 
+router.delete('/:id', (req, res) => {
+  res.status(200).json({message: `delete contact for ${req.params.id}`})
+}); 
 module.exports = router; 
