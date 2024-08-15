340f52da0ba73f411d8f3eae9d85288002705eb2
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-08-15 06:14:55 +0530

    errorHandler added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const errorHandler = require('./midleware/errorHandler');
 const dotenv = require('dotenv').config();
 const app = express();
 app.use(express.json());
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 
